e5cb8810af1d9b1c55a14d0d9f0f5b8486bdedfd
stuarttimwhite <stuart.white@spaceandtime.io>
2026-02-08 23:21:28 -0500

    temp

--- packages/cre-sdk/scripts/run-standard-tests.sh
+++ packages/cre-sdk/scripts/run-standard-tests.sh
@@ -17,5 +17,5 @@ fi
 # Copy standard test files to dist
 echo "Copying test files..." && cp -r ./src/standard_tests/* ./dist/workflows/standard_tests/
 
-# Run standard tests
-make standard_tests
+# # Run standard tests
+# make standard_tests
